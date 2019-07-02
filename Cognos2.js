a226e684f7e2942ed48b89b8695a5dee7c651900
Xdev9 <50918133+Xdev9@users.noreply.github.com>
2019-07-02 11:34:13 -0400

    Update and rename Cognos.js to Cognos2.js

--- Cognos2.js
+++ Cognos2.js
@@ -1,6 +1,10 @@
 //1. Define Require Module
-define(["jquery"], function(jQuery) {
+define(["https://api.tiles.mapbox.com/mapbox-gl-js/v0.29.0/mapbox-gl.js", "jquery"], function(mapboxgl, jQuery) {
     "use strict";
+    var map = '',
+        bounds = '',
+        geojsonFeature = {};
+
 
     //2. Create and Name Control
     function BasicControl() {};
