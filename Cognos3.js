72fc7f76740ba4b6e53ea4067c23c6c244e136ea
Xdev9 <50918133+Xdev9@users.noreply.github.com>
2019-07-02 11:50:06 -0400

    Update Cognos3.js

--- Cognos3.js
+++ Cognos3.js
@@ -1,20 +1,3 @@
-/*
- Author: 
- Ed Gonzalez  
- edgonzalez@icdata.io
- www.icdata.io 
- www.linkedin.com/in/ed-gonzalez-9683696
- 
-	Free for personal and commercial use under the CCA 3.0 license
-	I just ask for you give me credit for the code and tell your friends about it.
-	Note this is a very simple implementation, and is only meant to get you started using Custom Controls, and the MapBox API
-*/
-
-//Step 1 Add reference tp MapBox API, and to jQuery API, then add variables to be used 
-//Step 2 Add Initialize Code and set up default map. Make sure to add Map Token Key
-//Step 2a Make some minor adjustments to default map, and setup Bounds Variable
-
-
 define(["https://api.tiles.mapbox.com/mapbox-gl-js/v0.29.0/mapbox-gl.js", "jquery"], function(mapboxgl, jQuery) {
     "use strict";
 
