d4e536c2677fe599e8111e36e78acac524022c96
Xdev9 <50918133+Xdev9@users.noreply.github.com>
2019-07-02 11:57:54 -0400

    Update CognosTest.js

--- CognosTest.js
+++ CognosTest.js
@@ -1,6 +1,5 @@
-define(["https://api.tiles.mapbox.com/mapbox-gl-js/v0.29.0/mapbox-gl.js", "jquery"], function(mapboxgl, jQuery) {
+define(["https://api.tiles.mapbox.com/mapbox-gl-js/v0.29.0/mapbox-gl.js", "jquery"], function(mapboxgl, jquery) {
     "use strict";
-
     var map = '',
         bounds = '',
         geojsonFeature = {};
@@ -8,7 +7,8 @@ define(["https://api.tiles.mapbox.com/mapbox-gl-js/v0.29.0/mapbox-gl.js", "jquer
     function BasicControl() {};
 
 
-    BasicControl.prototype.initialize = function(oControlHost, fnDoneInitializing) {
+
+    BasicControl.prototype.initialize = function(oControlHost, fnDoneInitializing, oDataStore) {
 
         jQuery("head link[rel='stylesheet']").last().after("<link href='https://api.tiles.mapbox.com/mapbox-gl-js/v0.34.0/mapbox-gl.css' rel='stylesheet' />");
 
@@ -16,14 +16,14 @@ define(["https://api.tiles.mapbox.com/mapbox-gl-js/v0.29.0/mapbox-gl.js", "jquer
         var mapContainer = oControlHost.container.id;
 
         //*** Step 2a make some minor adjustments to default map */
-        mapboxgl.accessToken = 'pk.eyJ1IjoiZnhoYXdrIiwiYSI6ImNqaDZqYmVsajFwb3kycWs0dzM5aDFxbXgifQ.DcqavEFQJWPJ8eUAGLbK_A';// Map token Key
-            map = new mapboxgl.Map({
-                container: mapContainer,
-                style: 'mapbox://styles/mapbox/streets-v9',
-                center: [-96, 37.8], //Update Map Center to mid US
-                zoom: 3, //Change Default Zoom
-                interactive: true //Set Interactive to true
-            });
+        mapboxgl.accessToken = 'pk.eyJ1IjoiZnhoYXdrIiwiYSI6ImNqaDZqYmVsajFwb3kycWs0dzM5aDFxbXgifQ.DcqavEFQJWPJ8eUAGLbK_A'; //Make sure to add Map Token Key
+        var map = new mapboxgl.Map({
+            container: mapContainer,
+            style: 'mapbox://styles/mapbox/streets-v9',
+            center: [-96, 37.8], //Update Map Center to mid US
+            zoom: 3, //Change Default Zoom
+            interactive: true //Set Interactive to true
+        });
 
         //Set up the Bounds variable
         bounds = new mapboxgl.LngLatBounds();
@@ -42,10 +42,30 @@ define(["https://api.tiles.mapbox.com/mapbox-gl-js/v0.29.0/mapbox-gl.js", "jquer
 
 
     BasicControl.prototype.setData = function(oControlHost, oDataStore) {
+        console.log('2 Set Data')
+            //Default GeoJSON
+        geojsonFeature = {
+            "type": "FeatureCollection",
+            "features": []
+        }
 
-        console.log('SetData *****************')
+        var iRowCount = oDataStore.rowCount;
+        for (var iRow = 0; iRow < iRowCount; iRow++) {
+
+            var feature = {}
+            feature['type'] = 'Feature'
+            feature['geometry'] = {
+                'type': 'Point',
+                'coordinates': [parseFloat(oDataStore.getCellValue(iRow, 1)), parseFloat(oDataStore.getCellValue(iRow, 2))],
+            }
+            feature['properties'] = { 'name': oDataStore.getCellValue(iRow, 0) }
+            geojsonFeature['features'].push(feature)
+        }
+
+        console.log(geojsonFeature)
 
     };
+
 
 
     return BasicControl;
