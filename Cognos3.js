1fbf18cbf0a930b410eb61fdcd1a6b9eb90e733f
Xdev9 <50918133+Xdev9@users.noreply.github.com>
2019-07-02 11:48:40 -0400

    Update Cognos3.js

--- Cognos3.js
+++ Cognos3.js
@@ -25,30 +25,7 @@ define(["https://api.tiles.mapbox.com/mapbox-gl-js/v0.29.0/mapbox-gl.js", "jquer
     function BasicControl() {};
 
 
-    BasicControl.prototype.initialize = function(oControlHost, fnDoneInitializing) {
-
-        jQuery("head link[rel='stylesheet']").last().after("<link href='https://api.tiles.mapbox.com/mapbox-gl-js/v0.34.0/mapbox-gl.css' rel='stylesheet' />");
-
-
-        var mapContainer = oControlHost.container.id;
-
-        //*** Step 2a make some minor adjustments to default map */
-        mapboxgl.accessToken = 'pk.eyJ1IjoiZnhoYXdrIiwiYSI6ImNqaDZqYmVsajFwb3kycWs0dzM5aDFxbXgifQ.DcqavEFQJWPJ8eUAGLbK_A'; // Map token Key
-            map = new mapboxgl.Map({
-                container: mapContainer,
-                style: 'mapbox://styles/mapbox/streets-v9',
-                center: [-96, 37.8], //Update Map Center to mid US
-                zoom: 3, //Change Default Zoom
-                interactive: true //Set Interactive to true
-            });
-
-         //Tell Cognos that we are done initializing 
-        fnDoneInitializing();
-
-    };
-
-
-   
+       
 
 
     return BasicControl;
