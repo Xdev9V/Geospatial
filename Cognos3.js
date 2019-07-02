a322d5e503370ef8308ec2e63951b336f7c0622b
Xdev9 <50918133+Xdev9@users.noreply.github.com>
2019-07-02 11:43:20 -0400

    Update Cognos3.js

--- Cognos3.js
+++ Cognos3.js
@@ -1,27 +1,46 @@
-//1. Define Require Module
+/*
+ Author: 
+ Ed Gonzalez  
+ edgonzalez@icdata.io
+ www.icdata.io 
+ www.linkedin.com/in/ed-gonzalez-9683696
+ 
+	Free for personal and commercial use under the CCA 3.0 license
+	I just ask for you give me credit for the code and tell your friends about it.
+	Note this is a very simple implementation, and is only meant to get you started using Custom Controls, and the MapBox API
+*/
+
+//Step 1 Add reference tp MapBox API, and to jQuery API, then add variables to be used 
+//Step 2 Add Initialize Code and set up default map. Make sure to add Map Token Key
+//Step 2a Make some minor adjustments to default map, and setup Bounds Variable
+
+
 define(["https://api.tiles.mapbox.com/mapbox-gl-js/v0.29.0/mapbox-gl.js", "jquery"], function(mapboxgl, jQuery) {
     "use strict";
+
     var map = '',
         bounds = '',
         geojsonFeature = {};
 
+    function BasicControl() {};
 
-    //2. Create and Name Control
-    BasicControl.prototype.initialize = function(oControlHost, fnDoneInitializing, oDataStore) {
+
+    BasicControl.prototype.initialize = function(oControlHost, fnDoneInitializing) {
 
         jQuery("head link[rel='stylesheet']").last().after("<link href='https://api.tiles.mapbox.com/mapbox-gl-js/v0.34.0/mapbox-gl.css' rel='stylesheet' />");
+
 
         var mapContainer = oControlHost.container.id;
 
         //*** Step 2a make some minor adjustments to default map */
-        mapboxgl.accessToken = 'pk.eyJ1IjoiZnhoYXdrIiwiYSI6ImNqaDZqYmVsajFwb3kycWs0dzM5aDFxbXgifQ.DcqavEFQJWPJ8eUAGLbK_A'; //Make sure to add Map Token Key
-        map = new mapboxgl.Map({
-            container: mapContainer,
-            style: 'mapbox://styles/mapbox/streets-v9',
-            center: [-96, 37.8], //Update Map Center to mid US
-            zoom: 3, //Change Default Zoom
-            interactive: true //Set Interactive to true
-        });
+        mapboxgl.accessToken = 'pk.eyJ1IjoiZnhoYXdrIiwiYSI6ImNqaDZqYmVsajFwb3kycWs0dzM5aDFxbXgifQ.DcqavEFQJWPJ8eUAGLbK_A'; // Map token Key
+            map = new mapboxgl.Map({
+                container: mapContainer,
+                style: 'mapbox://styles/mapbox/streets-v9',
+                center: [-96, 37.8], //Update Map Center to mid US
+                zoom: 3, //Change Default Zoom
+                interactive: true //Set Interactive to true
+            });
 
         //Set up the Bounds variable
         bounds = new mapboxgl.LngLatBounds();
@@ -31,6 +50,20 @@ define(["https://api.tiles.mapbox.com/mapbox-gl-js/v0.29.0/mapbox-gl.js", "jquer
 
     };
 
-    //6. Return new Control 
+
+    BasicControl.prototype.draw = function(oControlHost) {
+
+        console.log('Draw ******************')
+
+    };
+
+
+    BasicControl.prototype.setData = function(oControlHost, oDataStore) {
+
+        console.log('SetData *****************')
+
+    };
+
+
     return BasicControl;
 });
