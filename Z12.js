e46f720e0e50c19fefb93adb3f379924af84fda2
Xdev9 <50918133+Xdev9@users.noreply.github.com>
2019-07-09 13:24:17 -0400

    Update and rename Z11.js to Z12.js

--- Z12.js
+++ Z12.js
@@ -1,4 +1,4 @@
-define(["https://api.tiles.mapbox.com/mapbox-gl-js/v0.45.0/mapbox-gl.js", "jquery"], function(mapboxgl, jQuery) {
+define(["https://api.tiles.mapbox.com/mapbox-gl-js/v1.1.0/mapbox-gl.js", "jquery"], function(mapboxgl, jQuery) {
     "use strict";
     var map = '',
         bounds = '',
@@ -11,7 +11,7 @@ define(["https://api.tiles.mapbox.com/mapbox-gl-js/v0.45.0/mapbox-gl.js", "jquer
 
     BasicControl.prototype.initialize = function(oControlHost, fnDoneInitializing, oDataStore) {
 
-        jQuery("head link[rel='stylesheet']").last().after("<link href='https://api.tiles.mapbox.com/mapbox-gl-js/v0.45.0/mapbox-gl.css' rel='stylesheet' />");
+        jQuery("head link[rel='stylesheet']").last().after("<link href='https://api.tiles.mapbox.com/mapbox-gl-js/v1.1.0/mapbox-gl.css' rel='stylesheet' />");
 
         var mapContainer = oControlHost.container.id;
 
