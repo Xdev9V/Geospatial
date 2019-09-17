284387eaebc72c07e38e735fea76484c55be256a
Xdev9 <50918133+Xdev9@users.noreply.github.com>
2019-09-17 19:10:31 -0400

    Create Page15.js

---
+++ Page15.js
@@ -0,0 +1,34 @@
+define( [], function(  ) {
+"use strict";
+
+
+function validate()
+{
+};
+
+validate.prototype.initialize = function( oControlHost, fnDoneInitializing )
+{
+
+var fntestId = function( aValues )
+{
+   //do work
+   
+   if (repromptIsNeeded){
+      oControlHost.reprompt();
+      return false;
+   }else{
+      return true;
+   }
+oControlHost.page.getControlByName('testId; ).setValidator( fntestId );
+
+
+fnDoneInitializing();
+	
+};
+
+
+   
+}
+};
+return validate;
+});
