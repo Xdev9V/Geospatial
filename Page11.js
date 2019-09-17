c9977452c53f4df22122950fa9028502bf3d80f1
Xdev9 <50918133+Xdev9@users.noreply.github.com>
2019-09-17 19:07:01 -0400

    Create Page11.js

---
+++ Page11.js
@@ -0,0 +1,20 @@
+define( function() {
+"use strict";
+
+function PageModule()
+{
+};
+
+PageModule.prototype.load = function( oPage )
+{
+
+	var oControl = oPage.getControlByName( "P_Property" ); //Replace Product Line with your prompt control name
+	var oValues = oControl.getValues(true);
+	
+	//oControl.addValues( [oValues[2]] ); // Used to control the value pre-selected in the prompt control
+};
+console.log ("***************prompt***************");
+console.log (oValues);
+
+return PageModule;
+});
